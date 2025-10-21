2fd28f5510fdf06788bf54067d0e44b811137fd6
kaiwxn <kwyang0620@icloud.com>
2025-10-21 08:56:24 +0200

    initial

---
+++ ela.py
@@ -0,0 +1,18 @@
+import torch
+
+"""
+# Add dataset (create binary labled images with and without artifacts)
+#
+# First test ELA on example images --> check, if it works as expected
+# 
+# Add a neural network to train ELA on the dataset
+#
+# Export the model
+"""
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
